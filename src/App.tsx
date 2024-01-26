9b9ceb3ba37c33ec33c4d1ea3b51cabf9cc5f031
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-01-26 18:09:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Home } from "./pages/Home/Home"
+import styles from './App.module.css'
+
+import './global.css'
+
+
+function App() {
+
+  return (
+    <main className={styles.container}>
+      <Home />
+    </main>
+  )
+}
+
+export default App
